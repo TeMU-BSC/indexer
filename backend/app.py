32cc3c70f74c75807412d7a6efa5075b0b4ea408
Ankush Rana <ankush.ran13@gmail.com>
2019-11-05 13:14:07 +0100

    Modify APP.py, It's not finished yet

--- backend/app.py
+++ backend/app.py
@@ -20,27 +20,113 @@ COLLECTION = DB[COLLECTION_NAME]
 APP = Flask(__name__)
 CORS(APP)
 
+ARTICLE_ID = "_id"
 ABSTRACT = "ab_es"
-DECSCODES = "decsCodes"
 DESCRIPTORS = "descriptors"
-DESCRIPTORS_OBJ = "descriptors_obj"
-ADDED_BY_ID = "addedBy"
+ADDED = "added"
+ADDED_BY_ID = "by"
+ADDED_ON = "on"
 
 
 def getDescriptorObj_decsList(jsonObj) -> (str, List[dict], List[str]):
+    """Ex of json object sent by user. "descriptors" contain a list of objects. This object in mongoDB's collection will same format as it.
+        {
+        "_id": "biblio-1001075",
+        "descriptors": [
+            {
+                "id": "11",
+                "added": [
+                    {
+                        "by": "A1",
+                        "on": "2019-11-02T08:02:36"
+                    }
+                ]
+            }
+        }
+    """
 
-    added_by_id_id = jsonObj[ADDED_BY_ID]
-    descriptors_dict_list = jsonObj[DESCRIPTORS]
+    article_id = jsonObj[ARTICLE_ID]
+    user_added_by = jsonObj[DESCRIPTORS][0][ADDED][0]
 
-    decsCodes = []
-    for descriptor in descriptors_dict_list:
-        decsCodes.append(descriptor['id'])
+    mongo_obj = COLLECTION.find_one({ARTICLE_ID: article_id})
 
-    return added_by_id_id, descriptors_dict_list, decsCodes
+    mongo_descriptors = mongo_obj.get(DESCRIPTORS)
+    jSon_descriptors = jsonObj[DESCRIPTORS]
+
+    if not mongo_descriptors:
+        return jSon_descriptors
+
+    for descriptor in jSon_descriptors:
+        """Example of each descriptor from json object.
+            "descriptors": [
+                    {
+                    "id": "11",
+                    "added": [
+                                {
+                                    "by": "A1",
+                                    "on": "2019-11-02T08:02:36"
+                                }
+                            ]
+                    },
+                     {
+                    "id": "12",
+                    "added": [
+                                {
+                                    "by": "A1",
+                                    "on": "2019-11-02T08:02:36"
+                                }
+                            ]
+                    }
+                ]
+        """
+        # Getting descriptor id from main json object, that have been sent by user to save.
+        user_descriptor_id = descriptor["id"]
+        """ EX:
+                "id": "122"
+        """
+        user_added_obj = descriptor[ADDED]  # Getting added object' list from main json object, that have been sent by user to save.
+        """ EX:
+                "added": [
+                            {
+                                "by": "A1",
+                                "on": "2019-11-02T08:02:36"
+                            },
+                            {
+                                "by": "A1",
+                                "on": "2019-11-02T08:02:36"
+                            }
+                        ]
+        """
+        for mongo_descriptor in mongo_descriptors:  # Loop to get each descriptor object from the list of sescriptors.
+            # descriptor id from mongo DB descriptors of each descriptor.
+            mongo_descriptor_id = mongo_descriptor.get("id")
+            """ EX:
+                    "id": "122"
+            """
+            if user_descriptor_id == mongo_descriptor_id:
+                mongo_added_list = mongo_descriptor[ADDED]
+                """ EX:
+                        "added": [
+                                    {
+                                        "by": "A1",
+                                        "on": "2019-11-02T08:02:36"
+                                    },
+                                    {
+                                        "by": "A2",
+                                        "on": "2019-11-02T08:05:36"
+                                    }
+                                ]
+                """
+                # for mongo_addedObj in mongo_added_list:
+                #     mongo_added_by = mongo_addedObj.get(ADDED_BY_ID)
+                #     if mongo_added_by == user_added_by:
+                #         mongo_added_by[ADDED_ON] = use
+
+            mongo_descriptor.append(descriptor)
 
 
 @APP.route('/articles', methods=['GET'])
-def get() -> List[dict] :
+def get() -> List[dict]:
     """The method with get request, it returns articles depeneding on the request. 
 
     """
@@ -51,17 +137,16 @@ def get() -> List[dict] :
     articles_list_output = []
     # data = request.get_json()
     # print(request.values)
+
     found_articles_cursor = COLLECTION.find()
     for article in found_articles_cursor:
-        tmp_dict = {"id": article["_id"],
-                    "abstractText": article.get(ABSTRACT),
-                    DECSCODES: article.get(DECSCODES),
+        tmp_dict = {ARTICLE_ID: article[ARTICLE_ID],
                     DESCRIPTORS: article.get(DESCRIPTORS)}
 
         articles_list_output.append(tmp_dict)
 
     articles_list_sorted = sorted(
-        articles_list_output, key=lambda k: (k['id']))
+        articles_list_output, key=lambda k: (k[ARTICLE_ID]))
     total_records_len = len(articles_list_sorted)
 
     start_record_to_send = args.get('start')
@@ -91,23 +176,20 @@ def get() -> List[dict] :
 @APP.route('/modify', methods=['PUT'])
 def put():
     """ The method server for a petitison of put. It receives 
-    
+
     :return: [description]
     :rtype: [type]
     """
     json_obj = request.json
-    article_id = json_obj['id']
+    article_id = json_obj[ARTICLE_ID]
 
-    added_by, descriptors_obj, decsCodes = getDescriptorObj_decsList(json_obj)
+    descriptors_obj = getDescriptorObj_decsList(json_obj)
 
-    key_to_set = str(DESCRIPTORS_OBJ + '.' + added_by)
+    COLLECTION.update_one({ARTICLE_ID: article_id},
+                          {"$set": {DESCRIPTORS: descriptors_obj}}
+                          )
 
-    COLLECTION.update_one({"_id": article_id},
-                          {"$set": {key_to_set: descriptors_obj},
-                           "$addToSet": {DECSCODES:{"$each":decsCodes}}
-                          })
-
-    return jsonify({key_to_set:descriptors_obj})
+    return jsonify(descriptors_obj)
 
     # return jsonify({'message': 'Hello from modify'})
 
