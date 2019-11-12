89abb37e44b481b873f44fc122845b4c544434a9
Ankush Rana <ankush.ran13@gmail.com>
2019-11-12 10:44:17 +0100

    All ready to test script

--- backend/app.py
+++ backend/app.py
@@ -22,30 +22,94 @@ COLLECTION = DB[COLLECTION_NAME]
 APP = Flask(__name__)
 CORS(APP)
 
+# Constants of attributes of article form mongoDB
 ARTICLE_ID = "_id"
-
 DESCRIPTORS = "descriptors"
 ADDED = "added"
 ADDED_BY_ID = "by"
 ADDED_ON = "on"
+DECS_ID = "id"
 
 
-def modifyDecriptors(jsonObj, descriptors_list):
-    """{
-        "decsCode": "101",
-        "removedBy": "A99",
-        "removedOn": 1573038300,
-        "articleId": "biblio-985342"
-    }"""
+def remove_from_Decriptors(jsonObj, descriptors_list):
+    """ The method removes descriptor's added object from the list added. (added list: It contains id of annotator and time of indexed)
+        It receives json object set by user and descriptors_list of DATA BASE, as parameters.
+        After it returns the modified list of descriptor, as new list. So it can save into DATA BASE directly.
 
-    decsCode = jsonObj["decsCode"]
+    :param jsonObj: A dict sent by user with information to delete a added object from the list.
+    :type jsonObj: dict
+    :return: Modified list of descriptors. The list may contain dict object inside.
+    :rType: list[]
+
+    .. highlight:: python
+
+       jsonObj = {
+            "decsCode": "101",
+            "removedBy": "A99",
+            "removedOn": 1573038300,
+            "articleId": "biblio-985342"
+            }
+    """
+
+   
+
+    decsCode = jsonObj["decsCode"]  # decsCode from json object, sent by user.
+
+    # annotator Id from json object,  sent by user.
     removed_by = jsonObj["removedBy"]
 
+    # Loop to run each descriptor from descriptors list. Than it can check one by one.
     for descriptor in descriptors_list:
-        if descriptor["id"] == decsCode:
-            for added in descriptor[ADDED]:
-                if added[ADDED_BY_ID] == removed_by:
-                    descriptor[ADDED].remove(added)
+
+        if descriptor[DECS_ID] == decsCode: #If user's decs Id match with decs Id from the descriptors list from DATA BASE.
+            for addedOne in descriptor[ADDED]:  #Loop f
+                if addedOne[ADDED_BY_ID] == removed_by:
+                    descriptor[ADDED].remove(addedOne)
+
+                if len(descriptor[ADDED]) == 0:
+                    descriptors_list.remove(descriptor)
+
+    return descriptors_list
+
+
+def add_to_Decriptors(jsonObj, descriptors_list):
+    """{
+        "decsCode": "101",
+        "addedBy": "A99",
+        "addedOn": 1573038300,
+        "articleId": "biblio-985342"
+        }"""
+
+    decsCode = jsonObj["decsCode"]
+    addedBy = jsonObj["addedBy"]
+    addedOn = jsonObj["addedOn"]
+
+    is_added = False
+
+    if descriptors_list:
+        for descriptor in descriptors_list:
+            if descriptor[DECS_ID] == decsCode:
+                descriptor[ADDED].append(
+                    {ADDED_BY_ID: addedBy, ADDED_ON: addedOn})
+                is_added = True
+                break
+
+        if not is_added:
+            descriptors_list.append({
+                DECS_ID: decsCode,
+                ADDED: [{
+                    ADDED_BY_ID: addedBy,
+                    ADDED_ON: addedOn
+                }]
+            })
+    else:
+        descriptors_list = [{
+            DECS_ID: decsCode,
+            ADDED: [{
+                ADDED_BY_ID: addedBy,
+                ADDED_ON: addedOn
+            }]
+        }]
 
     return descriptors_list
 
@@ -57,10 +121,9 @@ def get():
     """
     args = request.args
 
-    print(args)
     annotatorId = "A1"
-    articles_list_output = []
 
+    articles_list_output = []
     found_articles_cursor = COLLECTION.find()
 
     for article in found_articles_cursor:
@@ -74,14 +137,14 @@ def get():
                                        if added[ADDED_BY_ID] == annotatorId]
 
         tmp_dict = {"articleId": article[ARTICLE_ID],
-                    "tittle" : article["ti_es"],
+                    "tittle": article["ti_es"],
                     "abstractText": article["ab_es"],
                     DESCRIPTORS: descriptor_list_to_send}
 
         articles_list_output.append(tmp_dict)
 
     articles_list_sorted = sorted(
-        articles_list_output, key=lambda k: (k[ARTICLE_ID]))
+        articles_list_output, key=lambda k: (k["articleId"]))
     total_records_len = len(articles_list_sorted)
 
     start_record_to_send = args.get('start')
@@ -105,8 +168,8 @@ def get():
     return jsonify(articles_list_output[start_record_to_send: start_record_to_send + total_records_to_send])
 
 
-@APP.route('/modify', methods=['PUT'])
-def put():
+@APP.route('/delete', methods=['DELETE'])
+def delete():
     """ The method server for a petitison of put. It receives 
 
     :return: [description]
@@ -114,22 +177,40 @@ def put():
     """
 
     json_obj = request.json
-    article_id = json_obj["articleId"]
+    articleARTICLE_ID = json_obj["articleId"]
 
-    mongoObj = COLLECTION.find_one({"_id": article_id})
+    mongoObj = COLLECTION.find_one({ARTICLE_ID: articleARTICLE_ID})
 
     descriptors_list = mongoObj.get(DESCRIPTORS)
-    pprint.pprint(descriptors_list)
 
-    new_descriptors_list = modifyDecriptors(json_obj, descriptors_list)
+    new_descriptors_list = remove_from_Decriptors(json_obj, descriptors_list)
 
-    COLLECTION.update_one({ARTICLE_ID: article_id},
+    COLLECTION.update_one({ARTICLE_ID: articleARTICLE_ID},
                           {"$set": {DESCRIPTORS: new_descriptors_list}}
                           )
 
     return "done"
 
     # return jsonify({'message': 'Hello from modify'})
+
+
+@APP.route('/add', methods=['PUT'])
+def add():
+
+    json_obj = request.json
+    articleARTICLE_ID = json_obj["articleId"]
+
+    mongoObj = COLLECTION.find_one({ARTICLE_ID: articleARTICLE_ID})
+
+    descriptors_list = mongoObj.get(DESCRIPTORS)
+
+    new_descriptors_list = add_to_Decriptors(json_obj, descriptors_list)
+
+    COLLECTION.update_one({ARTICLE_ID: articleARTICLE_ID},
+                          {"$set": {DESCRIPTORS: new_descriptors_list}}
+                          )
+
+    return "done"
 
 
 if __name__ == '__main__':
