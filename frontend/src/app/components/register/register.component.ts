9d9ca11f585fb962770334d287a76e89b2e94723
Alejandro Asensio <alejandro.asensio.10@gmail.com>
2019-11-13 17:40:54 +0100

    added snackbar in register component

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core'
 import { AuthenticationService } from 'src/app/services/auth.service'
 import { Router } from '@angular/router'
 import { User } from 'src/app/app.model'
+import { MatSnackBar } from '@angular/material'
 
 @Component({
   selector: 'app-register',
@@ -16,25 +17,34 @@ export class RegisterComponent implements OnInit {
 
   constructor(
     public auth: AuthenticationService,
-    private router: Router
+    private router: Router,
+    private snackBar: MatSnackBar
   ) { }
 
   ngOnInit() {
   }
 
   register(user: User) {
-    this.auth.registerOne(user).subscribe(response => console.log(response))
+    this.auth.registerOne(user).subscribe(
+      response => this.user = response,
+      error => console.error(error),
+      () => this.snackBar.open(`Usuario registrado correctamente`, 'OK', { duration: 10000 })
+    )
   }
 
   bulkRegister() {
-    this.auth.registerMany(this.users).subscribe(response => console.log(response))
+    this.auth.registerMany(this.users).subscribe(
+      response => this.users = response,
+      error => console.error(error),
+      () => this.snackBar.open(`Usuarios registrados correctamente: ${this.users.length}`, 'OK', { duration: 10000 })
+    )
   }
 
   /**
    * Set the users attribute to bulk register all of these users.
    * @param event JSON file upload
    */
-  onFileChange(event) {
+  onFileSelected(event) {
     this.selectedFile = event.target.files[0]
     const fileReader = new FileReader()
     fileReader.readAsText(this.selectedFile, 'UTF-8')
