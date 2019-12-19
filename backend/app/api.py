fb5ebc8af331a0ee432187b29daf1a3970068261
Alejandro Asensio <alejandro.asensio.10@gmail.com>
2019-12-19 11:28:10 +0100

    Stop using the mongo container and change flask MONGO_URI to bsccnio01.bsc.es directly again

--- backend/app/api.py
+++ backend/app/api.py
@@ -19,11 +19,11 @@ from app import app
 
 
 # System
-# app.config['MONGO_URI'] = 'mongodb://mesinesp:mesinesp@bsccnio01.bsc.es:27017/BvSalud'
+app.config['MONGO_URI'] = 'mongodb://mesinesp:mesinesp@bsccnio01.bsc.es:27017/BvSalud'
 
 # Docker
 # app.config['MONGO_URI'] = 'mongodb://mesinesp:mesinesp@mongo:27017/BvSalud?authSource=admin'
-app.config['MONGO_URI'] = 'mongodb://mongo:27017/BvSalud'
+# app.config['MONGO_URI'] = 'mongodb://mongo:27017/BvSalud'
 
 app.config['JWT_SECRET_KEY'] = 'secret'
 mongo = PyMongo(app)
