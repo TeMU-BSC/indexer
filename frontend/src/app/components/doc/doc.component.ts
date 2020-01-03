55e6fe7a960343225e1ac31a659b632efcd9d0c5
Alejandro Asensio <alejandro.asensio.10@gmail.com>
2020-01-03 20:55:55 +0100

    Fix glitch in doc actions buttons (row layout in screens greater than xs)

--- frontend/src/app/components/doc/doc.component.ts
+++ frontend/src/app/components/doc/doc.component.ts
@@ -1,8 +1,10 @@
 import { Component, Input } from '@angular/core'
+import { MatSlideToggleChange } from '@angular/material'
+
 import { Doc } from 'src/app/app.model'
-import { MatSlideToggleChange, MatSnackBar } from '@angular/material'
 import { AppService } from 'src/app/services/app.service'
 import { AuthenticationService } from 'src/app/services/auth.service'
+
 
 @Component({
   selector: 'app-doc',
@@ -12,57 +14,27 @@ import { AuthenticationService } from 'src/app/services/auth.service'
 export class DocComponent {
 
   @Input() doc: Doc
-  // toggleText: string
 
   constructor(
     private appService: AppService,
-    private auth: AuthenticationService,
-    // private snackBar: MatSnackBar
+    private auth: AuthenticationService
   ) { }
 
   /**
-   * If doc is uncompleted, mark as completed; and the other way around.
-   */
-  // onChange(event: MatSlideToggleChange): void {
-  //   if (!this.doc.decsCodes.length) {
-  //     // Warn the user that DeCS list is empty
-  //     this.snackBar.open('Atención: El artículo debe tener al menos un DeCS para marcarlo como completado.', 'OK')
-  //     // and change back the toggle slide
-  //     // console.log(this.doc.completed)
-  //     // this.doc.completed = false
-  //   } else {
-  //     // Visualy toggle the completed property (true / false)
-  //     this.doc.completed = event.checked
-  //     // Make that change permanent to database
-  //     const docToMark = {
-  //       userId: this.auth.getUserDetails().identity.id,
-  //       docId: this.doc.id
-  //     }
-  //     if (this.doc.completed) {
-  //       this.appService.addCompletedDoc(docToMark).subscribe()
-  //     } else {
-  //       this.appService.removeCompletedDoc(docToMark).subscribe()
-  //     }
-  //   }
-  // }
-
-  /**
-   * If doc is uncompleted, mark as completed; and the other way around.
+   * Toggle completed status of a doc (completed/uncompleted).
    */
   onChange(event: MatSlideToggleChange): void {
-    // Visualy toggle the completed property (true / false)
+    // Visualy toggle the completed property (boolean)
     this.doc.completed = event.checked
 
-    // Make that change permanent to database
+    // Make that change permanent into database
     const docToMark = {
       userId: this.auth.getUserDetails().identity.id,
       docId: this.doc.id
     }
     if (this.doc.completed) {
-      // this.toggleText = 'Completado'
       this.appService.addCompletedDoc(docToMark).subscribe()
     } else {
-      // this.toggleText = 'Marcar como completado'
       this.appService.removeCompletedDoc(docToMark).subscribe()
     }
   }
