e68ad013d4d76a6720ea71a04c38e0d04d990c9f
Alejandro Asensio <alejandro.asensio.10@gmail.com>
2020-01-08 17:44:46 +0100

    Change baseUrl to production mode in api.ts

--- frontend/src/app/services/api.ts
+++ frontend/src/app/services/api.ts
@@ -1,11 +1,11 @@
 // Development
-const hostname = 'localhost'
-const port = 5000
-const baseEndpoint = ''
+// const hostname = 'localhost'
+// const port = 5000
+// const baseEndpoint = ''
 
 // Production
-// const hostname = 'bsccnio01.bsc.es'
-// const port = 80
-// const baseEndpoint = '/api'
+const hostname = 'bsccnio01.bsc.es'
+const port = 80
+const baseEndpoint = '/api'
 
 export const baseUrl = `http://${hostname}:${port}${baseEndpoint}`
