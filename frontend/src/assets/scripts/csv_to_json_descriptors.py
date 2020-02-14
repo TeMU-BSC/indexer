925c2b4f79a8a4a2eb5256ae3a9769e953c02d88
Alejandro Asensio <alejandro.asensio.10@gmail.com>
2020-02-14 17:04:08 +0100

    Generate all-decs json file with python script (a part of the tsv file)

--- frontend/src/assets/scripts/csv_to_json_descriptors.py
+++ frontend/src/assets/scripts/csv_to_json_descriptors.py
@@ -9,9 +9,6 @@ with open(csvfilepath) as csvfile:
     reader = csv.DictReader(csvfile, dialect=csv.excel_tab)
     descriptors = [row for row in reader]
 
-    # Print the short terms in Spanish
-    [print(descriptor.get('termSpanish')) for descriptor in descriptors if len(descriptor.get('termSpanish')) <= 3]
-
 # Write data to a JSON file
 with open(jsonfilepath, 'w') as jsonfile:
     jsonfile.write(json.dumps(descriptors))
