0199a0f7b6ccbe68d1834def59465d7a295c4dc3
Alejandro Asensio <alejandro.asensio.10@gmail.com>
2020-03-04 13:01:29 +0100

    Uncomment apiUrl 'http://bsccnio01.bsc.es:80/api'

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: true,
-  // apiUrl: 'http://bsccnio01.bsc.es:80/api'
-  // apiUrl: 'http://localhost:80/api'
-  apiUrl: 'http://84.88.51.174:80/api'
+  apiUrl: 'http://bsccnio01.bsc.es:80/api'
 }
