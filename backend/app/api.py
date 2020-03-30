9b9594f5dad00c658297a1f0fccdbf7ebfd85726
Alejandro Asensio <alejandro.asensio.10@gmail.com>
2020-03-30 16:23:02 +0200

    Sanity check on development set selection

--- backend/app/api.py
+++ backend/app/api.py
@@ -27,8 +27,10 @@ app.config['MONGO_URI'] = environ.get('MONGO_URI')
 bcrypt = Bcrypt(app)
 mongo = PyMongo(app)
 CORS(app)
+
 SEED_INITIAL_VALUE = 777
 DEVELOPMENT_SET_LENGTH = 750
+ABSTRACT_MINIMUM_LENGTH = 200
 
 @app.route('/hello')
 def hello():
@@ -590,17 +592,15 @@ def extract_development_set(strategy):
     validations = list(mongo.db.validations.find({'user': {'$in': annotator_ids}}, {'_id': 0}))
     validated_docs = [doc for validation in validations for doc in validation.get('docs')]
     double_validated_docs = [doc_id for doc_id, count in Counter(validated_docs).items() if count == 2]
-    random.seed(SEED_INITIAL_VALUE)
-    selected_docs = random.sample(double_validated_docs, DEVELOPMENT_SET_LENGTH)
 
-    # get the decs codes (with union approach, this is the same as set)
-    selected_annotations = list(mongo.db.annotationsValidated.find({'doc': {'$in': selected_docs}}, {'_id': 0}))
+    # get the decs codes for each document
+    double_validated_annotations = list(mongo.db.annotationsValidated.find({'doc': {'$in': double_validated_docs}}, {'_id': 0}))
     annotations = defaultdict(list)
-    for annotation in selected_annotations:
+    for annotation in double_validated_annotations:
         annotations[annotation.get('doc')].append(annotation.get('decsCode'))
 
     # prepare the codes depending on the strategy
-    final_selection = list()
+    chosen_annotations = list()
     macro_common = 0
     macro_unique = 0
     micro_accumulate = list()
@@ -613,7 +613,7 @@ def extract_development_set(strategy):
             selected_codes = list(unique)
         if strategy == 'intersection':
             selected_codes = [code for code, count in Counter(codes).items() if count == 2]
-        final_selection.append({'doc': doc, 'codes': selected_codes})
+        chosen_annotations.append({'doc': doc, 'codes': selected_codes})
         # calculate the agreement
         common = 0
         for count in Counter(codes).values():
@@ -629,28 +629,34 @@ def extract_development_set(strategy):
     print('macro:', macro)
     print('micro:', micro)
 
-    # get the texts and anonymize the doc_ids
+    # get the texts ignoring the ones with short abstracts and anonymize the doc_ids 
     selected_importants = list(mongo.db.selected_importants.find({}))
     reec_clinical_cases = list(mongo.db.reecClinicalCases.find({}))
     isciii_projects = list(mongo.db.isciiiProjects.find({}))
     all_docs = selected_importants + reec_clinical_cases + isciii_projects
+    random.seed(SEED_INITIAL_VALUE)
     development_set = list()
-    for index, item in enumerate(final_selection, start=1):
-        for doc in all_docs:
-            if doc.get('_id') == item.get('doc'):
-                title = doc.get('ti_es')
-                abstract = doc.get('ab_es')
-                journal = doc.get('ta')[0] if doc.get('ta') else None
-                db = doc.get('db')
-                year = doc.get('entry_date').year if doc.get('entry_date') else None
+    for n in range(1, DEVELOPMENT_SET_LENGTH + 1):
+        while True:    
+            choice = random.choice(chosen_annotations)
+            chosen_annotations.remove(choice)
+            for doc in all_docs:
+                if doc.get('_id') == choice.get('doc'):
+                    title = doc.get('ti_es')
+                    abstract = doc.get('ab_es')
+                    journal = doc.get('ta')[0] if doc.get('ta') else None
+                    db = doc.get('db')
+                    year = doc.get('entry_date').year if doc.get('entry_date') else None
+            if len(abstract) >= ABSTRACT_MINIMUM_LENGTH:
+                break
         development_set.append({
-            'id': f'mesinesp-dev-{index:0{len(str(DEVELOPMENT_SET_LENGTH))}}',
+            'id': f'mesinesp-dev-{n:0{len(str(DEVELOPMENT_SET_LENGTH))}}',
             'title': title,
             'abstractText': abstract,
             'journal': journal,
             'db': db,
             'year': year,
-            'decsCodes': item.get('codes')
+            'decsCodes': choice.get('codes')
         })
 
     return jsonify(development_set)
