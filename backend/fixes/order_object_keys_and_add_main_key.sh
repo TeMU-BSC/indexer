0d98e96e8ca5fa8c4f32a031bd47165e4d3cda5d
Alejandro Asensio <alejandro.asensio.10@gmail.com>
2020-05-11 16:59:19 +0200

    refactor: arbitrarily reorder python script and jq with sed shell commands

--- backend/fixes/order_object_keys_and_add_main_key.sh
+++ backend/fixes/order_object_keys_and_add_main_key.sh
@@ -1,2 +1,5 @@
+# https://stedolan.github.io/jq/
+# https://stedolan.github.io/jq/manual/v1.6/
+
 sudo apt install jq
 cat file.json | jq -c -S '.[]' | sed '1s/^/{"articles":[\n/; $s/$/\n]}/; $!s/$/,/'
